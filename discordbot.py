3d3330b6aced7b9140ec82aea4a633f18f82f833
nobutaka-yoshio <75005442+nobutaka-yoshio@users.noreply.github.com>
2020-11-25 16:58:43 +0900

    ああああああああ

--- discordbot.py
+++ discordbot.py
@@ -7,33 +7,33 @@ import asyncio
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-
-
-# @bot.event
-# async def on_command_error(ctx, error):
-#     orig_error = getattr(error, "original", error)
-#     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-#     await ctx.send(error_msg)
-
-
-# @bot.command()
-# async def じゃんけん(ctx):
-#     await ctx.send('pong')
-
 arr = ['ぐー:punch:', 'ちょき:v:', 'ぱー:hand_splayed:']
-# 接続に必要なオブジェクトを生成
-client = discord.Client()
+
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
 
 
-# メッセージ受信時に動作する処理
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
+@bot.command()
+async def じゃんけん(ctx):
+    await ctx.send("「"+random.choice(arr)+"」を出しました。")
 
-    if message.content == 'じゃんけん':
-        await message.channel.send(message.author.mention+" は「"+random.choice(arr)+"」を出しました。")
+# arr = ['ぐー:punch:', 'ちょき:v:', 'ぱー:hand_splayed:']
+# # 接続に必要なオブジェクトを生成
+# client = discord.Client()
+
+
+# # メッセージ受信時に動作する処理
+# @client.event
+# async def on_message(message):
+#     # メッセージ送信者がBotだった場合は無視する
+#     if message.author.bot:
+#         return
+
+#     if message.content == 'じゃんけん':
+#         await message.channel.send(message.author.mention+" は「"+random.choice(arr)+"」を出しました。")
 
 
 bot.run(token)
