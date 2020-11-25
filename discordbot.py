dba85fe6891092b796ea5f6d6aefe8efeaf35d4c
nobutaka-yoshio <75005442+nobutaka-yoshio@users.noreply.github.com>
2020-11-25 16:13:45 +0900

    編集

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,8 @@
 from discord.ext import commands
 import os
 import traceback
+import discord
+import random
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -16,6 +18,29 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
+# 自分のBotのアクセストークンに置き換えてください
+TOKEN = os.environ['DISCORD_BOT_TOKEN']
+arr = ['ぐー:punch:', 'ちょき:v:', 'ぱー:hand_splayed:']
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+
+    if message.content == 'じゃんけん':
+        await message.channel.send(message.author.mention+" は「"+random.choice(arr)+"」を出しました。")
 
 
 bot.run(token)
