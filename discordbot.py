273bb8597e5aa153402bc7e3f8521d5070ceb6e5
nobutaka-yoshio <75005442+nobutaka-yoshio@users.noreply.github.com>
2020-11-25 16:50:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,16 +9,16 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+# @bot.event
+# async def on_command_error(ctx, error):
+#     orig_error = getattr(error, "original", error)
+#     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+#     await ctx.send(error_msg)
 
 
-@bot.command()
-async def じゃんけん(ctx):
-    await ctx.send('pong')
+# @bot.command()
+# async def じゃんけん(ctx):
+#     await ctx.send('pong')
 
 arr = ['ぐー:punch:', 'ちょき:v:', 'ぱー:hand_splayed:']
 # 接続に必要なオブジェクトを生成
